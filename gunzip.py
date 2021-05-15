97b5e30fb2307a5c6795abd7e4755e3504652396
Louis Kronberg <louiskronberg@gmail.com>
2021-05-15 15:37:12 +0200

    add support for static huffman encoded blocks, refactoring
    	o removed verbose arguemnt
    	o replaced FLG enum class with integer constants
    	o new integer constants for different block types
    	o put @dataclasses at the beginning of the file
    	o refactor and simplify inflate
    	o distinguish in __main__ what block to decode
    	o add support for static huffman encoded blocks

--- gunzip.py
+++ gunzip.py
@@ -2,16 +2,53 @@
 from dataclasses import dataclass
 from enum import IntFlag
 from argparse import ArgumentParser
+import sys
 
 rle_alphabet = [16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15]
 
-length_offset = {257:  3, 258:  4, 259:  5, 260:  6, 261:  7, 262:  8, 263:  9, 264: 10,
-                 265: 11, 266: 13, 267: 15, 268: 17, 269: 19, 270: 23, 271: 27,
-                 272: 31, 273: 35, 274: 43, 275: 51, 276: 59, 277: 67, 278: 83,
-                 279: 99, 280: 115, 281: 131, 282: 163, 283: 195, 284: 227, 285: 258}
+length_extra_bits   = [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 0]
+distance_extra_bits = [0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13]
 
-distance_offset = [1, 2, 3, 4, 5, 7, 9, 13, 17, 25, 33, 49, 65, 97, 129, 193, 257, 385, 513, 769, 1025, 1537,
-                   2049, 3073, 4097, 6145, 8193, 12289, 16385, 24577]
+length_offset   = [3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 15, 17, 19, 23, 27, 31, 35, 43, 51, 59, 67, 83, 99, 115, 131, 163, 195, 227, 258]
+distance_offset = [1, 2, 3, 4, 5, 7, 9, 13, 17, 25, 33, 49, 65, 97, 129, 193, 257, 385, 513, 769, 1025, 1537, 2049, 3073, 4097, 6145, 8193, 12289, 16385, 24577]
+
+fixed_huf_codelengths = 144*[8] + 112*[9] + 24*[7] + 8*[8]
+fixed_litlen_alphabet = range(288)
+
+NO_COMPRESSION = 0
+FIXED          = 1
+DYNAMIC        = 2
+RESREVED       = 3
+
+FTEXT          = 1
+FHCRC          = 2
+FEXTRA         = 4
+FNAME          = 8
+FCOMMENT       =16
+
+@dataclass
+class DeflateHeader:
+
+    bfinal: int
+    btype:  int
+    hlit:   int
+    hdist:  int
+    hclen:  int
+
+@dataclass
+class GzipHeader:
+
+    magic:        tuple
+    cm:           int
+    flg:          int
+    mtime:        int
+    xfl:          int
+    os:           int
+    xlen:         int     = 0
+    extra_bytes:  int     = 0
+    fname:        str     = None
+    comment:      str     = None
+    crc:          int     = 0
 
 def take(it, count):
     for _ in range(count):
@@ -44,8 +81,8 @@ def construct_huffman(alphabet, code_lengths):
             characters.append(character)
     
     # recreate huffman codes from code lenghts
-    huffman_code = dict()
     min_code = 0
+    huffman_code = dict()
     max_bitwidth = max(char_by_code_len) + 1
 
     for length in range(max_bitwidth):
@@ -100,74 +137,42 @@ def inflate(stream, litlen_huf_code, dist_huf_code):
     # read in from literal/len alphabet until end of block symbol
     while (symbol := decode_huf(stream, litlen_huf_code)) != 256:
 
-        if symbol > 256:
-
-            # parse length symbol
-            length   = 0
-            if 265 <= symbol < 285:
-                extra_bits  = (symbol - 261) // 4
-                length      = to_number(stream, extra_bits)
-            length  += length_offset[symbol]
-
-            # parse backwards distance symbol
-            distance    = 0
-            dist_symbol = decode_huf(stream, dist_huf_code)
-            if 4 <= dist_symbol:
-                extra_bits  = (dist_symbol - 2) // 2
-                distance    = to_number(stream, extra_bits)
-            distance += distance_offset[dist_symbol]
-
-            # inflate length, distance pair
-            if distance <= length:
-                data_slice = uncompressed_data[-distance:]
-                for i in range(length):
-                    uncompressed_data.append(data_slice[i % distance])
-            else:
-                uncompressed_data += uncompressed_data[-distance:-distance+length]
-
-        else:
+        if symbol < 257:
             # output literal
             uncompressed_data.append(symbol)
+            print(f"literal {hex(symbol)}")
+        else:
+            # parse length symbol
+            length   = to_number(stream, length_extra_bits[symbol - 257])
+            length  += length_offset[symbol - 257]
+
+            # parse backwards distance symbol
+            dist_symbol = decode_huf(stream, dist_huf_code)
+            distance    = to_number(stream, distance_extra_bits[dist_symbol])
+            distance   += distance_offset[dist_symbol]
+
+            # inflate length, distance pair
+            data_slice = uncompressed_data[-distance:]
+            for i in range(length):
+                uncompressed_data.append(data_slice[i % distance])
+
+            print(f"match {length}, {distance}")
 
     return uncompressed_data
 
-@dataclass
-class DeflateHeader:
-
-    bfinal: int
-    btype:  int
-    hlit:   int 
-    hdist:  int
-    hclen:  int
-
-class FLG(IntFlag):
-    FTEXT   = 1
-    FHCRC   = 2
-    FEXTRA  = 4
-    FNAME   = 8
-    FCOMMENT=16
-
-@dataclass
-class GzipHeader:
-
-    magic:      tuple
-    cm:         int
-    flg:        int
-    mtime:      int
-    xfl:        int
-    os:         int
-    xlen:       int     = 0
-    xlenbytes:  int     = 0
-    fname:      str     = None
-    comment:    str     = None
-    crc:        int     = 0
 
 def deflate_header_from_stream(stream):
-    return DeflateHeader( to_number(stream, 1),
-                          to_number(stream, 2),
-                          to_number(stream, 5),
-                          to_number(stream, 5),
-                          to_number(stream, 4))
+
+    bfinal = to_number(stream, 1)
+    btype =  to_number(stream, 2)
+    hlit = hdist = hclen = 0
+
+    if btype == DYNAMIC:
+        hlit = to_number(stream, 5)
+        hdist = to_number(stream, 5)
+        hclen = to_number(stream, 4)
+
+    return DeflateHeader(bfinal, btype, hlit, hdist, hclen)
 
 def gzip_header_from_stream(stream):
 
@@ -179,17 +184,17 @@ def gzip_header_from_stream(stream):
     os      = to_number(stream, 8)
 
     kwargs = dict()
-    if flg & FLG.FEXTRA:
-        kwargs['xlen']      = to_number(stream, 8)
-        kwargs['xlenbytes'] = to_number(stream, kwargs['xlen'])
+    if flg & FEXTRA:
+        kwargs['xlen']          = to_number(stream, 16)
+        kwargs['extra_bytes']   = to_number(stream, kwargs['xlen'])
 
-    if flg & FLG.FNAME:
+    if flg & FNAME:
         kwargs['fname'] = decode_cstring(stream)
 
-    if flg & FLG.FCOMMENT:
+    if flg & FCOMMENT:
         kwargs['comment'] = decode_cstring(stream)
 
-    if flg & FLG.FHCRC:
+    if flg & FHCRC:
         kwargs['crc'] = to_number(stream, 8)
 
     return GzipHeader(magic, cm, flg, mtime, xfl, os, **kwargs)
@@ -205,33 +210,54 @@ if __name__ == '__main__':
 
     parser = ArgumentParser(description='small gunzip implementation')
     parser.add_argument('--file', required=True, type=str, help='file to unzip' )
-    parser.add_argument('--verbose', action='store_const', const=True, default=False, help='verbose output')
     args = parser.parse_args()
 
     with open(args.file, 'rb') as fobj:
         gz = fobj.read()
 
-    stream = bitstream(gz)
-
+    stream     = bitstream(gz)
     gz_header  = gzip_header_from_stream(stream)
     def_header = deflate_header_from_stream(stream)
 
-    # read in code lengths for the code alphabet
-    rle_codelengths = [to_number(stream, 3) for _ in range(4 + def_header.hclen)]
+    uncompressed_data = bytes()
 
-    # construct huffman tree to decode code lengths for literal/length and distance alphabets
-    huf_code = construct_huffman(rle_alphabet, rle_codelengths)
-    
-    # decode (huffman + run-length) encoded codelengths for literal/length and distance huffman code
-    litlen_codelengths = decode_codelengths(stream, huf_code, 257 + def_header.hlit)
-    dist_codelengths   = decode_codelengths(stream, huf_code, def_header.hdist + 1)
-    
-    # recreate huffman codes from the code lenghts
-    litlen_huf_code = construct_huffman(range(257 + def_header.hlit), litlen_codelengths)
-    dist_huf_code   = construct_huffman(range(def_header.hdist + 1), dist_codelengths)
+    if def_header.btype == DYNAMIC:
 
-    # inflate the date using the previously constructed huffman codes
-    uncompressed_data = inflate(stream, litlen_huf_code, dist_huf_code)
+        # read in code lengths for the code alphabet
+        rle_codelengths = [to_number(stream, 3) for _ in range(4 + def_header.hclen)]
+        huf_code = construct_huffman(rle_alphabet, rle_codelengths)
+
+        # decode 257 + hlit + hdist + 1 codelengths
+        litlen_codelengths = decode_codelengths(stream, huf_code, def_header.hlit  + 257)
+        dist_codelengths   = decode_codelengths(stream, huf_code, def_header.hdist + 1)
+
+        # recreate huffman codes from the code lenghts
+        litlen_huf_code = construct_huffman(range(def_header.hlit  + 257), litlen_codelengths)
+        dist_huf_code   = construct_huffman(range(def_header.hdist + 1), dist_codelengths)
+
+        # inflate the date using the previously constructed huffman codes
+        uncompressed_data = inflate(stream, litlen_huf_code, dist_huf_code)
+
+        print("Huffman Code for Codelengths")
+        print_huffman_code(huf_code)
+
+        print("Huffman Code for Literal/Length Symbols")
+        print_huffman_code(litlen_huf_code)
+
+        print("Huffman Code for Distance Symbols")
+        print_huffman_code(dist_huf_code)
+
+    elif def_header.btype == FIXED:
+
+        litlen_fixed_huf_code = construct_huffman(fixed_litlen_alphabet, fixed_huf_codelengths)
+        dist_huf_code = { (5, dist_code): dist_code  for dist_code in range(32) }
+
+        uncompressed_data = inflate(stream, litlen_fixed_huf_code, dist_huf_code)
+
+    elif def_header.btype == NO_COMPRESSION:
+        sys.exit("Error: btype = 0 not implemented yet")
+    else:
+        sys.exit("Error: invalid block type")
 
     # write the uncompressed data to disk
     outfilename = args.file.removesuffix('.gz')
@@ -241,11 +267,3 @@ if __name__ == '__main__':
 
     with open(outfilename, 'wb') as fobj:
         fobj.write(uncompressed_data)
-
-    if args.verbose:
-        print("Huffman Code for Codelengths")
-        print_huffman_code(huf_code)
-        print("Huffman Code for Literal/Length Symbols")
-        print_huffman_code(litlen_huf_code)
-        print("Huffman Code for Distance Symbols")
-        print_huffman_code(dist_huf_code)
